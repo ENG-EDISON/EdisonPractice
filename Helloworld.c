ebef2bd3df28ba656e8e96c066b9944d00e51de2
Edison Ngunjiri <edisonngunjiri@gmail.com>
2022-08-27 12:13:42 -0700

    Adde a c file that printf Hellowolrd number of time entered by user

---
+++ Helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int a=0;
+
+int main()
+{
+  printf("Add the number of times to print Helloworld\r\n");
+  scanf("%d",&a);
+  for(int i=0;i<a;i++)
+    {
+      printf("Helloword\r\n");
+    }
+}
